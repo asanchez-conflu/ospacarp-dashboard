618e38702aa2d01c32ee57e92bf6bcda0e4f2a41
Andres Sanchez <asanchez@confluenciait.com>
2025-01-03 15:40:14 -0300

    Ajuste de layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
         <meta name='viewport' content='width=device-width, initial-scale=1' />
       </head>
       <body className={`${inter.className} min-h-screen text-black`}>
-        <div className='flex flex-col md:flex-row min-h-screen'>
+        <div className='flex flex-row min-h-screen'>
           <SideNavigation />
           <main className='flex-grow p-4 overflow-y-auto bg-gray-100	'>
             {children}
--- src/components/common/SideNavigation.tsx
+++ src/components/common/SideNavigation.tsx
@@ -9,42 +9,49 @@ import {
 
 const Sidebar = () => {
   return (
-    <aside className='p-4 bg-white'>
+    <aside className='p-4 bg-white w-[250px] 2xl:w-[300px]'>
       <div className='flex flex-col items-center mb-4'>
-        <div className='w-16 h-16 rounded-full flex items-center justify-center mr-4 bg-blue-500'>
+        <div className='w-16 h-16 rounded-full flex items-center justify-center bg-blue-500'>
           <span className='text-2xl font-semibold'>MO</span>
         </div>
-        <div>
+        <div className='text-center'>
           <h2 className='text-lg font-semibold'>Milton Olivera</h2>
           <p className='text-sm'>Sistemas</p>
         </div>
       </div>
 
-      <Link href='/' className='flex items-center w-full py-2 mb-2 rounded-md'>
-        <MdHome className='mr-2' />
-        Inicio
-      </Link>
-      <Link
-        href='/ingresos'
-        className='flex items-center w-full py-2 mb-2 rounded-md'
-      >
-        <MdTrendingUp className='mr-2' />
-        Ingresos
-      </Link>
-      <Link
-        href='/egresos'
-        className='flex items-center w-full py-2 mb-2 rounded-md'
-      >
-        <MdArrowDownward className='mr-2' />
-        Egresos
-      </Link>
-      <Link
-        href='/afiliados'
-        className='flex items-center w-full py-2 mb-2 rounded-md'
-      >
-        <MdPeople className='mr-2' />
-        Afiliados
-      </Link>
+      <hr className='my-4 border-gray-200' />
+
+      <div className='flex flex-col items-center'>
+        <Link
+          href='/'
+          className='flex items-center w-[190px] px-5 py-2 mb-2 rounded-full bg-blue-600'
+        >
+          <MdHome className='mr-2' />
+          Inicio
+        </Link>
+        <Link
+          href='/ingresos'
+          className='flex items-center w-[190px] px-5 py-2 mb-2 rounded-full'
+        >
+          <MdTrendingUp className='mr-2' />
+          Ingresos
+        </Link>
+        <Link
+          href='/egresos'
+          className='flex items-center w-[190px] px-5 py-2 mb-2 rounded-full'
+        >
+          <MdArrowDownward className='mr-2' />
+          Egresos
+        </Link>
+        <Link
+          href='/afiliados'
+          className='flex items-center w-[190px] px-5 py-2 mb-2 rounded-full'
+        >
+          <MdPeople className='mr-2' />
+          Afiliados
+        </Link>
+      </div>
     </aside>
   );
 };
