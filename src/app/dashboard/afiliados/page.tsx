29333dba0104b716ff52d86dcf854eab59e8e477
Andres Sanchez <asanchez@confluenciait.com>
2025-03-18 14:15:33 -0300

    Correccion de formato de meses

--- src/app/dashboard/afiliados/page.tsx
+++ src/app/dashboard/afiliados/page.tsx
@@ -241,7 +241,7 @@ const AfiliadosPage: React.FC = () => {
 
   // Formatea Trend Data para los gráficos
   const convertTrendDataTyped = (trendData: TrendItem[]) => {
-    const labels = trendData.map((item) => item.monthName);
+    const labels = trendData.map((item) => item.monthName.substring(0, 3));
     const data = trendData.map((item) => parseInt(item.count, 10)); // Parse count to number
 
     return {
